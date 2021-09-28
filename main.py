09fdc97cfb8348538f461160934784e004ff4a86
jfran20 <jfranm99@gmail.com>
2021-09-27 21:19:51 -0500

    Paso 1: Completado
    Se agrego el script completo y los archivos generados con el texto de los pdf con un poco de pre-procesado.
    
    Esto solo se ha testeado con Windows, debido al uso de funciones os no estoy muy seguro de su implementacion en algun otro SO

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+import os
 from tika import parser 
 
-raw = parser.from_file('PDF/BIMBO 2021-2.pdf')
-print(raw['content'])
+## GENERAR ARCHIVOS TXT POR CADA PDF EN EL SUBDIRECTORIO PDF
+files = len(os.listdir('./PDF')) 
+for i,my_file in enumerate(os.listdir('PDF')):
+    print(i + 1 ,'/',files,'Procesando archivo:', my_file)
+
+    # Leemos el pdf y obtenemos el texto
+    try:
+        raw = parser.from_file('PDF/' + my_file)
+        print('Conversion exitosa')
+    except:
+        print('Se detecto un fallo con el archivo:',my_file)
+        print('Por favor asegurate de tener Java instalado')
+        break
+
+    # Realizamos la escritura a un archivo txt
+    new_path = './Preprocesados/' + my_file.strip('.pdf') + '.txt'
+    with open(new_path, 'w',encoding='utf8') as f:
+        f.write(raw['content'])
+    print('Escritura exitosa')
+
+    # Eliminamos lineas en blanco
+    f = open(new_path,'r',encoding='utf8').readlines()
+    with open(new_path,'w',encoding='utf8') as new_f:
+        for line in f:
+            if line.split():
+                new_f.write(line)
+    print('Preprocesado exitoso \n')
